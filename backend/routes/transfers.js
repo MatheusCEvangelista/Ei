536bb31bfc36d186b46c06401cabdd7c4daac98c
estagio7 <estagio7@chb.local>
2026-07-29 09:41:46 -0300

    Teste

--- backend/routes/transfers.js
+++ backend/routes/transfers.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express.Router();
 const { createClient } = require('@supabase/supabase-js');
 const authMiddleware   = require('../middleware/auth');
-const { v4: uuidv4 }  = require('uuid');
+const { v4: uuidv4 }  = require('crypto');
 
 router.use(authMiddleware);
 
